bf1f4a496fee98a44f1dc02ba889206ecfc166c6
Miguel López <miguelopezv@gmail.com>
2018-03-25 13:42:19 -0500

    Initial commit, with interface for iOs and Android

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace MyTrips.Droid
+{
+    [Activity(Label = "MyTrips", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        EditText usernameEditText, passwordEditText;
+        Button loginButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            usernameEditText.FindViewById<EditText>(Resource.Id.usernameEditText);
+            passwordEditText.FindViewById<EditText>(Resource.Id.passwordEditText);
+            loginButton.FindViewById<Button>(Resource.Id.loginButton);
+           
+            loginButton.Click += onClickLogin;
+        }
+
+        private void onClickLogin(object sender, EventArgs args) { 
+            if (string.IsNullOrEmpty(usernameEditText.Text) || string.IsNullOrEmpty(passwordEditText.Text))
+            {
+
+            } else {
+                //TODO: Go to main page
+            }
+        }
+    }
+}
+
---
+++ iOS/ViewController.designer.cs
@@ -0,0 +1,53 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace MyTrips.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIButton Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField passwordTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField usernameTextField { get; set; }
+
+        [Action ("UIButton206_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton206_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (loginButton != null) {
+                loginButton.Dispose ();
+                loginButton = null;
+            }
+
+            if (passwordTextField != null) {
+                passwordTextField.Dispose ();
+                passwordTextField = null;
+            }
+
+            if (usernameTextField != null) {
+                usernameTextField.Dispose ();
+                usernameTextField = null;
+            }
+        }
+    }
+}
