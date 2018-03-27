6c49709db66cecfa942f621f7cbfab3df43b00cd
Miguel López <miguelopezv@gmail.com>
2018-03-26 22:05:10 -0500

    Add generic method to insert in DB

--- MyTrips/Classes/DbHelper.cs
+++ MyTrips/Classes/DbHelper.cs
@@ -6,5 +6,19 @@ namespace MyTrips.Classes
         public DbHelper()
         {
         }
+
+        public static bool insert<T>(ref T item, string dbRoute)
+        {
+            using (SQLite.SQLiteConnection conection = new SQLite.SQLiteConnection(dbRoute))
+            {
+                conection.CreateTable<T>();
+
+                if(conection.Insert(item) > 0) {
+                    return true;
+                };
+
+            }
+            return false;
+        }
     }
 }
