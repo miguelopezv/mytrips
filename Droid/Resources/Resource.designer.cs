233aec823cf430fe098f882bd5a16e1022d3fc68
Miguel López <miguelopezv@gmail.com>
2018-03-26 22:05:27 -0500

    Multiple changes

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -44,11 +44,23 @@ namespace MyTrips.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int departureDatePicker = 2131034116;
+			
 			// aapt resource value: 0x7f050002
 			public const int loginButton = 2131034114;
 			
 			// aapt resource value: 0x7f050001
 			public const int passwordEditText = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int placeEditText = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int returnDatePicker = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int saveButton = 2131034118;
 			
 			// aapt resource value: 0x7f050000
 			public const int usernameEditText = 2131034112;
@@ -68,6 +80,9 @@ namespace MyTrips.Droid
 			
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int NewTrip = 2130903041;
 			
 			static Layout()
 			{
