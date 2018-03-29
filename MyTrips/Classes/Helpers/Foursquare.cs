724f497539e2ed1c5cc9234e790990ba18ad565d
Miguel López <miguelopezv@gmail.com>
2018-03-29 18:52:09 -0500

    add Helper to get both categories and search results

--- MyTrips/Classes/Helpers/Foursquare.cs
+++ MyTrips/Classes/Helpers/Foursquare.cs
@@ -2,29 +2,44 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
-using MyTrips.Classes.Helpers;
 using Newtonsoft.Json;
 
-namespace MyTrips.Classes
+namespace MyTrips.Classes.Helpers
 {
     public class Foursquare
     {
-        public async Task<List<Categories.Category>> getCategories() 
+        public async Task<List<Category>> getCategories() 
         {
-            List<Categories.Category> categories = new List<Categories.Category>();
+            List<Category> categories = new List<Category>();
 
-            var url = Constants.ObtainCategoriesURL();
+            string url = Constants.ObtainCategoriesURL();
 
             using(HttpClient client = new HttpClient())
             {
-                var answer = await client.GetStringAsync(url);
+                string answer = await client.GetStringAsync(url);
                 Categories response = JsonConvert.DeserializeObject<Categories>(answer);
 
                 categories = response.response.categories;
-
             }
 
             return categories;
+        }
+
+        public async Task<List<Venue>> getSearchResult()
+        {
+            List<Venue> venues = new List<Venue>();
+
+            string url = Constants.ObtainSearchURL("Chicago, Fl");
+
+            using(HttpClient client = new HttpClient())
+            {
+                string answer = await client.GetStringAsync(url);
+                SearchResult response = JsonConvert.DeserializeObject<SearchResult>(answer);
+
+                venues = response.response.venues;
+            }
+
+            return venues;
         }
     }
 }
