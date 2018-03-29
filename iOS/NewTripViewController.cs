3c7b9f6a2491469b179eb6c0fbdaa267ed3a69a0
Miguel López <miguelopezv@gmail.com>
2018-03-29 11:42:30 -0500

    method to save trip after button click

--- iOS/NewTripViewController.cs
+++ iOS/NewTripViewController.cs
@@ -1,6 +1,9 @@
 using Foundation;
+using MyTrips.Classes;
 using System;
+using System.IO;
 using UIKit;
+using GlobalToast;
 
 namespace MyTrips.iOS
 {
@@ -8,6 +11,36 @@ namespace MyTrips.iOS
     {
         public NewTripViewController (IntPtr handle) : base (handle)
         {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+            saveBarButton.Clicked += SaveBarButton_Clicked;
+		}
+
+        void SaveBarButton_Clicked(object sender, EventArgs e)
+        {
+            string fileName = "trips_db.sqlite";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library");
+            string fullPath = Path.Combine(filePath, fileName);
+
+            var newTrip = new Trip()
+            {
+                Place = placeTextField.Text,
+                DepartureDate = (DateTime)departureDatePicker.Date,
+                ReturnDate = (DateTime)returnDatePicker.Date
+
+            };
+
+            if(DbHelper.insert<Trip>(ref newTrip, fullPath))
+            {
+                NavigationController.PopViewController(true);
+            } else {
+                Toast.ShowToast("There was an error, try again");
+                Console.WriteLine("Error");
+            }
         }
     }
 }
