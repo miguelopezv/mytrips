b0d03dd0272c0fbe603b4d55e788c8f97c8ea370
Miguel López <miguelopezv@gmail.com>
2018-03-29 11:41:51 -0500

    Add new view and controller for the ListView

--- iOS/ListTripsViewController.cs
+++ iOS/ListTripsViewController.cs
@@ -1,13 +1,59 @@
 using Foundation;
+using MyTrips.Classes;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using UIKit;
 
 namespace MyTrips.iOS
 {
     public partial class ListTripsViewController : UITableViewController
     {
+        List<Trip> ListTrips;
+
         public ListTripsViewController (IntPtr handle) : base (handle)
         {
         }
-    }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            ListTrips = new List<Trip>();
+
+            NavigationItem.SetHidesBackButton(true, false);
+            NavigationController.SetNavigationBarHidden(false, true);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+
+            string fileName = "trips_db.sqlite";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library");
+            string fullPath = Path.Combine(filePath, fileName);
+
+            ListTrips = DbHelper.returnTrips(fullPath);
+
+            TableView.ReloadData();
+
+        }
+
+		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+		{
+            var cell = tableView.DequeueReusableCell("tripReuseIdentifier", indexPath);
+            var data = ListTrips[indexPath.Row];
+
+            cell.TextLabel.Text = data.Place;
+            cell.DetailTextLabel.Text = $"{data.DepartureDate:d} - {data.ReturnDate:d}";
+
+
+			return cell;
+		}
+
+		public override nint RowsInSection(UITableView tableView, nint section)
+		{
+            return ListTrips.Count;
+		}	
+	}
 }
--- iOS/ListTripsViewController.designer.cs
+++ iOS/ListTripsViewController.designer.cs
@@ -14,5 +14,16 @@ namespace MyTrips.iOS
     [Register ("ListTripsViewController")]
     partial class ListTripsViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem addItemButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (addItemButton != null) {
+                addItemButton.Dispose ();
+                addItemButton = null;
+            }
+        }
     }
 }
