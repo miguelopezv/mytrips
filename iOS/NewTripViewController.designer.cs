751f566e3bcf1b7b3b8ef99f78f420b170fd8359
Miguel López <miguelopezv@gmail.com>
2018-03-29 11:58:05 -0500

    Misc. iOS

--- iOS/NewTripViewController.designer.cs
+++ iOS/NewTripViewController.designer.cs
@@ -14,5 +14,43 @@ namespace MyTrips.iOS
     [Register ("NewTripViewController")]
     partial class NewTripViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIDatePicker departureDatePicker { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField placeTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIDatePicker returnDatePicker { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem saveBarButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (departureDatePicker != null) {
+                departureDatePicker.Dispose ();
+                departureDatePicker = null;
+            }
+
+            if (placeTextField != null) {
+                placeTextField.Dispose ();
+                placeTextField = null;
+            }
+
+            if (returnDatePicker != null) {
+                returnDatePicker.Dispose ();
+                returnDatePicker = null;
+            }
+
+            if (saveBarButton != null) {
+                saveBarButton.Dispose ();
+                saveBarButton = null;
+            }
+        }
     }
 }
