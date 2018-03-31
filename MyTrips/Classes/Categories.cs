89113882970065b6b085a8422cf692e703576577
Miguel López <miguelopezv@gmail.com>
2018-03-30 19:48:54 -0500

    modified toString method to return category name

--- MyTrips/Classes/Categories.cs
+++ MyTrips/Classes/Categories.cs
@@ -26,6 +26,11 @@ namespace MyTrips.Classes
             public string pluralName { get; set; }
             public string shortName { get; set; }
             public Icon icon { get; set; }
+
+            public override string ToString()
+            {
+                return name;
+            }
         }
 
         public class Icon
@@ -33,5 +38,5 @@ namespace MyTrips.Classes
             public string prefix { get; set; }
             public string suffix { get; set; }
         }
-    }
+	}
 }
