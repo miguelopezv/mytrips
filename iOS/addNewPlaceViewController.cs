ccf077825ce58fb6c18693d1d46dbd44553f27b4
Miguel López <miguelopezv@gmail.com>
2018-04-02 21:18:35 -0500

    display data

--- iOS/addNewPlaceViewController.cs
+++ iOS/addNewPlaceViewController.cs
@@ -3,6 +3,8 @@ using MyTrips.Classes;
 using MyTrips.Classes.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using UIKit;
 
 namespace MyTrips.iOS
@@ -10,6 +12,9 @@ namespace MyTrips.iOS
     public partial class addNewPlaceViewController : UIViewController
     {
         List<Categories.Category> categories;
+        List<Venue> venues;
+        public string selectedCity;
+        public int selectedCityId;
 
         public addNewPlaceViewController (IntPtr handle) : base (handle)
         {
@@ -20,16 +25,67 @@ namespace MyTrips.iOS
 		{
 			base.ViewDidLoad();
 
+            placesTableView.AllowsMultipleSelection = true;
+
             Foursquare helper = new Foursquare();
             categories = await helper.getCategories();
 
             var model = new CategoriesPickerViewModel(categories);
             model.selected_Category += Model_Selected_Category;
             categoriesPickerView.Model = model;
+
+            saveNewBarButton.Clicked += SaveNewBarButton_Clicked;
+
+            var category = model.idSelectedCategory;
+            venues = await helper.getVenues(selectedCity, category);
+
+            var tableSource = new TableSource(venues);
+            placesTableView.Source = tableSource;
+            placesTableView.ReloadData();
+
+            filterTextField.EditingChanged += FilterTextField_EditingChanged;
 		}
 
-        void Model_Selected_Category(object sender, EventArgs e)
+        async void Model_Selected_Category(object sender, EventArgs e)
         {
+            var category = (sender as CategoriesPickerViewModel).idSelectedCategory;
+
+            Foursquare helper = new Foursquare();
+            venues = await helper.getVenues(selectedCity, category);
+
+            var tableSource = new TableSource(venues);
+            placesTableView.Source = tableSource;
+            placesTableView.ReloadData();
+        }
+
+        void SaveNewBarButton_Clicked(object sender, EventArgs e)
+        {
+            string fileName = "trips_db.sqlite";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library");
+            string fullPath = Path.Combine(filePath, fileName);
+
+            var selectedItems = placesTableView.IndexPathsForSelectedRows;
+
+            foreach (var item in selectedItems)
+            {
+                var cellText = placesTableView.CellAt(item).TextLabel.Text;
+                var selectedVenue = venues.Where(v => v.name.Contains(cellText)).FirstOrDefault();
+
+                InterestSite interestSite = new InterestSite(selectedVenue, selectedCityId);
+
+                DbHelper.insert(ref interestSite, fullPath);
+            }
+
+            NavigationController.PopViewController(true);
+        }
+
+        void FilterTextField_EditingChanged(object sender, EventArgs e)
+        {
+            var filteredVenues = venues.Where(v => v.name.ToLower().Contains(filterTextField.Text.ToLower())).ToList();
+
+            var tableSource = new TableSource(filteredVenues);
+            placesTableView.Source = tableSource;
+            placesTableView.ReloadData();
         }
 
     }
@@ -73,4 +129,30 @@ namespace MyTrips.iOS
             selected_Category(this, null);
 		}
 	}
+
+    public class TableSource : UITableViewSource
+    {
+        public TableSource(List<Venue> places)
+        {
+            this.places = places;
+        }
+
+        List<Venue> places;
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("venueCellIdentifier");
+            var data = places[indexPath.Row];
+
+            cell.TextLabel.Text = data.name;
+            cell.DetailTextLabel.Text = data.categories.FirstOrDefault().name;
+
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return places.Count;
+        }
+    }
 }
