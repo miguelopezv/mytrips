36ed3f29d205127a747e1766945795bb8e3e78ba
Miguel López <miguelopezv@gmail.com>
2018-04-02 21:33:45 -0500

    Add Google Maps API Key

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -183,6 +183,9 @@ namespace MyTrips.Droid
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int GoogleMapsAPI = 2130968578;
+			
 			// aapt resource value: 0x7f040001
 			public const int app_name = 2130968577;
 			
