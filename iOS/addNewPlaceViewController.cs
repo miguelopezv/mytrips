73642cda4ef82056e0912433d501753c52b9a48b
Miguel López <miguelopezv@gmail.com>
2018-04-02 19:11:57 -0500

    Add new controller for new place to add

--- iOS/addNewPlaceViewController.cs
+++ iOS/addNewPlaceViewController.cs
@@ -1,13 +1,76 @@
 using Foundation;
+using MyTrips.Classes;
+using MyTrips.Classes.Helpers;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace MyTrips.iOS
 {
     public partial class addNewPlaceViewController : UIViewController
     {
+        List<Categories.Category> categories;
+
         public addNewPlaceViewController (IntPtr handle) : base (handle)
         {
+            
         }
+
+		public override async void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+            Foursquare helper = new Foursquare();
+            categories = await helper.getCategories();
+
+            var model = new CategoriesPickerViewModel(categories);
+            model.selected_Category += Model_Selected_Category;
+            categoriesPickerView.Model = model;
+		}
+
+        void Model_Selected_Category(object sender, EventArgs e)
+        {
+        }
+
     }
+
+    public class CategoriesPickerViewModel : UIPickerViewModel
+    {
+        List<Categories.Category> categories;
+        public string idSelectedCategory
+        {
+            get;
+            set;
+        }
+
+        public event EventHandler selected_Category;
+
+
+        public CategoriesPickerViewModel(List<Categories.Category> categories)
+        {
+            this.categories = categories;
+            idSelectedCategory = categories[0].id;
+        }
+
+		public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+		{
+            return categories.Count;
+		}
+
+		public override nint GetComponentCount(UIPickerView pickerView)
+		{
+            return 1;
+		}
+
+		public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+		{
+            return categories[(int)row].name;
+		}
+
+		public override void Selected(UIPickerView pickerView, nint row, nint component)
+		{
+            idSelectedCategory = categories[(int)row].id;
+            selected_Category(this, null);
+		}
+	}
 }
--- iOS/addNewPlaceViewController.designer.cs
+++ iOS/addNewPlaceViewController.designer.cs
@@ -14,5 +14,47 @@ namespace MyTrips.iOS
     [Register ("addNewPlaceViewController")]
     partial class addNewPlaceViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIPickerView categoriesPickerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField filterTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView placesTableView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem saveNewBarButton { get; set; }
+
+        [Action ("SaveNew_Activated:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void SaveNew_Activated (UIKit.UIBarButtonItem sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (categoriesPickerView != null) {
+                categoriesPickerView.Dispose ();
+                categoriesPickerView = null;
+            }
+
+            if (filterTextField != null) {
+                filterTextField.Dispose ();
+                filterTextField = null;
+            }
+
+            if (placesTableView != null) {
+                placesTableView.Dispose ();
+                placesTableView = null;
+            }
+
+            if (saveNewBarButton != null) {
+                saveNewBarButton.Dispose ();
+                saveNewBarButton = null;
+            }
+        }
     }
 }
