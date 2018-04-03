75dff1fe52ab63e2f587d0ab0d50a532caabcecc
Miguel López <miguelopezv@gmail.com>
2018-04-02 19:11:13 -0500

    Make Storyboard compatible despite new Xcode

---
+++ iOS/addNewPlaceViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MyTrips.iOS
+{
+    public partial class addNewPlaceViewController : UIViewController
+    {
+        public addNewPlaceViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ iOS/newPlacesViewController_.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MyTrips.iOS
+{
+    public partial class newPlacesViewController : UITableViewController
+    {
+        public newPlacesViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
